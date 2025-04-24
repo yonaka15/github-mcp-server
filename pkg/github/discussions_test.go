81547f4e608105ad814542138e220da8c0724172
Xavier RENE-CORAIL <xcorail@github.com>
2025-04-24 12:13:16 -0700

    Add tests for GitHub discussions support.

---
+++ pkg/github/discussions_test.go
@@ -0,0 +1,28 @@
+package github
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/google/go-github/v69/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_ListDiscussions(t *testing.T) {
+	// Test implementation here
+}
+
+func Test_GetDiscussion(t *testing.T) {
+	// Test implementation here
+}
+
+func Test_CreateDiscussion(t *testing.T) {
+	// Test implementation here
+}
+
+func Test_AddDiscussionComment(t *testing.T) {
+	// Test implementation here
+}
